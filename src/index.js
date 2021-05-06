80162700dbd509e25d2d8f71249cdac416f45bf6
Patrick <patrick095@gmail.com>
2021-05-06 17:58:23 -0300

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
+const PORT = process.env.PORT;
 const app = require("./app");
 
 app.listen(PORT, () =>{
